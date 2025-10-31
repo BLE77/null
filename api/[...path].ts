48c22ede95bd1920f572629326cbcc3ed79b6760
Ble77 <blessnft@gmail.com>
2025-10-30 19:15:07 -0700

    Fix API route import path for Vercel

--- api/[...path].ts
+++ api/[...path].ts
@@ -1,15 +1,18 @@
 import type { IncomingMessage, ServerResponse } from "http";
-import { createApp } from "../server/app";
+import { createApp } from "../server/app.js";
 
 let appPromise: ReturnType<typeof createApp> | null = null;
 
 async function getApp() {
-  if (!appPromise) appPromise = createApp();
+  if (!appPromise) {
+    const bundle = await createApp();
+    appPromise = bundle.app;
+  }
   return appPromise;
 }
 
 export default async function handler(req: IncomingMessage, res: ServerResponse) {
-  const { app } = await getApp();
+  const app = await getApp();
   // @ts-ignore Express handler signature
   return app(req, res);
 }
