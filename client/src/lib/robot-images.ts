af210e459718286ab228be5e31a379649da73666
Ble77 <blessnft@gmail.com>
2025-10-31 16:22:34 -0700

    Align product media with new attached assets

--- client/src/lib/robot-images.ts
+++ client/src/lib/robot-images.ts
@@ -1,21 +1,22 @@
 // Robot figure images for product display
 // Map current product names to robot figure assets bundled in attached_assets
 export const robotImageMap: Record<string, string> = {
-  "BYTE ME": "/attached_assets/fit 1_1761538260908.png",
-  "CLANKERS TOKYO": "/attached_assets/fit 2a_1761538260906.png",
-  "X402 TIE": "/attached_assets/fit 3a_1761538260907.png",
-  "X402 CALL TEE": "/attached_assets/fit 4a_1761538260907.png",
-  "CLANKERS BMX HOODIE": "/attached_assets/fit 5_1761538260907.png",
-  "PROVE YOU'RE NOT HUMAN": "/attached_assets/fit 8_1761538260907.png",
+  "BYTE ME": "/attached_assets/RobotByte.png",
+  "CLANKERS TOKYO": "/attached_assets/RobotTokyo.png",
+  "X402 TIE": "/attached_assets/RobotTie.png",
+  "X402 CALL TEE": "/attached_assets/RobotCall.png",
+  "CLANKERS BMX HOODIE": "/attached_assets/RobotGTA.png",
+  "PROVE YOU'RE NOT HUMAN": "/attached_assets/RobotProve.png",
 };
 
 // Additional robot figures that aren't mapped yet
 export const extraRobotImages = {
-  fit6: "/attached_assets/fit6_1761538260908.png",
-  fit7: "/attached_assets/fit 7_1761538260907.png",
-  fit8: "/attached_assets/fit 8_1761538260907.png",
-  fit9: "/attached_assets/fit 9_1761538260908.png",
-  human1: "/attached_assets/human 1_1761538260908.png",
+  streetCall: "/attached_assets/StreetCall.png",
+  streetCall2: "/attached_assets/StreetCall2.png",
+  streetGTA: "/attached_assets/StreetGTA.png",
+  streetTie: "/attached_assets/StreetTie.mp4",
+  streetTokyo: "/attached_assets/StreetTokyo.mp4",
+  human1: "/attached_assets/human 1_1761538225955.png",
   figure2: "/attached_assets/figure 2_1761538260908.png",
 };
 
