caf0dc006651aaa274cdb47064418cc7247e70bb
Ble77 <blessnft@gmail.com>
2025-11-02 15:36:36 -0800

    Forward admin product routes to Express

---
+++ api/admin/products/[...path].ts
@@ -0,0 +1,9 @@
+import type { IncomingMessage, ServerResponse } from "http";
+import handler from "../../[...path].js";
+
+export default function adminProductsHandler(
+  req: IncomingMessage,
+  res: ServerResponse,
+) {
+  return handler(req, res);
+}
